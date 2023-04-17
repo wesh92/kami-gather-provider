44da2ddb86d56f1c3a9609552b0f69e3d9bc66b8
Wes Hahn <52631646+wesh92@users.noreply.github.com>
2023-04-17 14:49:50 -0400

    updating/modernizing the unpack method

--- src/v1/collect.py
+++ src/v1/collect.py
@@ -29,7 +29,7 @@ class MarketData:
 
         region_data = {}
         for region, url in self.get_urls():
-            log(f"Requesting {region} market data...")
+            log(30, f"Requesting {region} market data...")
             response = requests.post(url, headers=headers, json=request_data, timeout=300)
             region_data[region] = unpack(response.content)
 
--- src/v1/utils/unpack.py
+++ src/v1/utils/unpack.py
@@ -1,5 +1,6 @@
-# Ganked from: https://github.com/shrddr/huffman_heap/blob/main/unpack.py
+# Ganked and updated from: https://github.com/shrddr/huffman_heap/blob/main/unpack.py
 
+import heapq
 import io
 import struct
 from collections import Counter
@@ -8,143 +9,231 @@ from bitstring import BitArray
 
 
 class Node:
-    def __init__(self, char, freq, left=None, right=None) -> None:
+    def __init__(self, char: str, freq: int, left: "Node" = None, right: "Node" = None) -> None:
         self.c = char
         self.f = freq
         self.left = left
         self.right = right
 
-    def __lt__(self, other):  # noqa: ANN204
+    def __lt__(self, other: "Node") -> bool:
         return self.f < other.f
 
-    def __le__(self, other):  # noqa: ANN204
+    def __le__(self, other: "Node") -> bool:
         return self.f <= other.f
 
-    def __repr__(self):  # noqa: ANN204
-        return f'{self.c}:{self.f}'
+    def __repr__(self) -> str:
+        return f"{self.c}:{self.f}"
 
 
-class MinHeap:
+class PriorityQueue:
+    """
+    A priority queue implementation using the heapq module.
+
+    Methods
+    -------
+    push(obj: Node) -> None
+        Push a Node object onto the priority queue.
+    pop() -> Node
+        Pop and return the Node object with the lowest frequency from the priority queue.
+    size() -> int
+        Return the number of elements in the priority queue.
+    """
+
     def __init__(self) -> None:
-        self.arr = []
+        self.heap = []
 
-    def size(self):  # noqa: ANN201
-        return len(self.arr)
+    def push(self, obj: Node) -> None:
+        """
+        Push a Node object onto the priority queue.
 
-    def push(self, obj):  # noqa: ANN201
-        self.arr.append(obj)
-        child_idx = self.size() - 1
+        Parameters
+        ----------
+        obj : Node
+            The Node object to push onto the priority queue.
+        """
+        heapq.heappush(self.heap, obj)
 
-        while True:
-            parent_idx = (child_idx - 1) // 2
-            if self.arr[parent_idx] <= self.arr[child_idx]:
-                return
+    def pop(self) -> Node:
+        """
+        Pop and return the Node object with the lowest frequency from the priority queue.
 
-            self.arr[parent_idx], self.arr[child_idx] = self.arr[child_idx], self.arr[parent_idx]
-            child_idx = parent_idx
+        Returns
+        -------
+        Node
+            The Node object with the lowest frequency.
+        """
+        return heapq.heappop(self.heap)
 
-            if child_idx <= 0:
-                return
+    def size(self) -> int:
+        """
+        Return the number of elements in the priority queue.
 
-    def pop(self):  # noqa: ANN201
-        obj = self.arr[0]
-
-        last = self.arr.pop()
-        if self.size() == 0:
-            return obj
-        self.arr[0] = last
-
-        parent_idx = 0
-        child_idx = 2 * parent_idx + 1
-        while child_idx < self.size():
-            if child_idx + 1 < self.size() and self.arr[child_idx + 1] < self.arr[child_idx]:
-                child_idx += 1
-
-            if self.arr[parent_idx] <= self.arr[child_idx]:
-                return obj
-
-            self.arr[parent_idx], self.arr[child_idx] = self.arr[child_idx], self.arr[parent_idx]
-
-            parent_idx = child_idx
-            child_idx = 2 * child_idx + 1
-
-        return obj
+        Returns
+        -------
+        int
+            The number of elements in the priority queue.
+        """
+        return len(self.heap)
 
 
-def make_tree(freqs):  # noqa: ANN201
-    h = MinHeap()
+def make_tree(freqs: dict[str, int]) -> Node:
+    pq = PriorityQueue()
     for c, f in freqs.items():
-        h.push(Node(c, f))
+        pq.push(Node(c, f))
 
-    while h.size() > 1:
-        a = h.pop()
-        b = h.pop()
+    while pq.size() > 1:
+        a = pq.pop()
+        b = pq.pop()
         n = Node(a.c + b.c, a.f + b.f, a, b)
-        h.push(n)
+        pq.push(n)
 
-    return h.pop()
+    return pq.pop()
 
 
-def decode(tree, freqs, packed, bits, verbose=False, check_stats=False):  # noqa: ANN201
+def decode(
+    tree: Node,
+    freqs: dict[str, int],
+    packed: bytes,
+    bits: int,
+    verbose: bool = False,
+    check_stats: bool = False,
+) -> str:
+    """
+    Decode the packed data using the given Huffman tree and character frequencies.
+
+    Parameters
+    ----------
+    tree : Node
+        The root node of the Huffman tree.
+    freqs : dict[str, int]
+        A dictionary containing character frequencies.
+    packed : bytes
+        The packed data to decode.
+    bits : int
+        The number of bits in the packed data.
+    verbose : bool, optional, default: False
+        If True, print the binary representation of the packed data.
+    check_stats : bool, optional, default: False
+        If True, verify that the decoded data matches the input character frequencies.
+
+    Returns
+    -------
+    str
+        The decoded data as a string.
+    """
     p = BitArray(bytes=packed)
     p = p[:bits]
     if verbose:
         print(p.bin)  # noqa: T201
-    unpacked = ''
+    unpacked = ""
     pos = 0
     while pos < len(p):
         node = tree
         while True:
             if pos >= len(p):
-                raise ValueError(f'invalid tree: out of message bounds, {unpacked=}')
+                raise ValueError(f"invalid tree: out of message bounds, {unpacked=}")
             bit = p[pos]
             node = node.right if bit else node.left
             pos += 1
             if node is None:
-                raise ValueError(f'invalid tree: dead end while walking, {unpacked=}')
+                raise ValueError(f"invalid tree: dead end while walking, {unpacked=}")
             if node.left is None and node.right is None:
                 break
         unpacked += node.c
 
     if check_stats:
         stats = Counter(unpacked)
-        for c, f in freqs.items():
-            if stats[c] != f:
-                raise ValueError(f"incorrect '{c}' freq: header={f} processed={stats[c]}, {unpacked=}")
+    for c, f in freqs.items():
+        if stats[c] != f:
+            raise ValueError(f"incorrect '{c}' freq: header={f} processed={stats[c]}, {unpacked=}")
 
     return unpacked
 
 
-def read(file, fmt):  # noqa: ANN201
+def read(file: io.BufferedReader, fmt: str) -> int | bytes:
+    """
+    Read and unpack data from a file according to the specified format.
+
+    Parameters
+    ----------
+    file : io.BufferedReader
+        The input file to read from.
+    fmt : str
+        The format string to use when unpacking data.
+
+    Returns
+    -------
+    int | bytes
+        The unpacked data, either as an integer or bytes.
+    """
     i = struct.calcsize(fmt)
     ret = struct.unpack(fmt, file.read(i))
-    if type(ret) == tuple and len(ret) == 1:
+    if isinstance(ret, tuple) and len(ret) == 1:
         return ret[0]
     return ret
 
 
-def get_freqs(file):  # noqa: ANN201
-    file_len, always0, chars_count = read(file, 'III')
+def get_freqs(file: io.BufferedReader) -> dict[str, int]:
+    """
+    Extract character frequencies from a file.
+
+    Parameters
+    ----------
+    file : io.BufferedReader
+        The input file containing the character frequencies.
+
+    Returns
+    -------
+    dict[str, int]
+        A dictionary containing character frequencies.
+    """
+    file_len, always0, chars_count = read(file, "III")
     freqs = {}
     for _i in range(chars_count):
-        count = read(file, 'I')
-        char = read(file, 'cxxx').decode('ascii')
+        count = read(file, "I")
+        char = read(file, "cxxx").decode("ascii")
         freqs[char] = count
     return freqs
 
 
-def unpack_file(file):  # noqa: ANN201
+def unpack_file(file: io.BufferedReader) -> str:
+    """
+    Unpack and decode data from a file.
+
+    Parameters
+    ----------
+    file : io.BufferedReader
+        The input file containing the packed data and character frequencies.
+
+    Returns
+    -------
+    str
+        The unpacked and decoded data as a string.
+    """
     freqs = get_freqs(file)
     tree = make_tree(freqs)
 
-    packed_bits, packed_bytes, unpacked_bytes = read(file, 'III')
+    packed_bits, packed_bytes, unpacked_bytes = read(file, "III")
 
     packed = file.read(packed_bytes)
     return decode(tree, freqs, packed, packed_bits)
 
 
-def unpack(data):  # noqa: ANN201
-    if type(data) == bytes:
+def unpack(data: bytes | io.BytesIO) -> str:
+    """
+    Unpack and decode data from a bytes object or a BytesIO object.
+
+    Parameters
+    ----------
+    data : bytes | io.BytesIO
+        The input data containing the packed data and character frequencies, either as bytes or a BytesIO object.
+
+    Returns
+    -------
+    str
+        The unpacked and decoded data as a string.
+    """
+    if isinstance(data, bytes):
         data = io.BytesIO(data)
 
     return unpack_file(data)
